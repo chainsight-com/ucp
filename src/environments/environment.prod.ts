c70f80f0e8a226d22dd37956215ec4aac1033265
John Lin <john.lin0420@gmail.com>
2021-01-23 09:48:44 +0800

    ch url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://api.unblock-analysis.com',
+  baseApiUrl: 'https://api2.unblock-analysis.com',
   googleOAuthClientId: '1086750536934-rfnbr5soh8dk7cqtujeubcgm6cejo2j6.apps.googleusercontent.com'
 };
